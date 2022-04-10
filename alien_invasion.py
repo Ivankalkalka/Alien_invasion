448e97e7690a25ef6d017665d976f5a0dd5dd976
Ivankalkalka <kivanka1551@gmail.com>
2022-04-10 17:17:25 +0300

    alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings_1 import Settings1
 
 class AlienInvasion:
 
@@ -9,18 +10,18 @@ class AlienInvasion:
         
         '''Initiate game,create game resources'''
         pygame.init()
+        self.settings = Settings1()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_heights))
         pygame.display.set_caption ('Alien invasion')
-        self.bg_color = (230,230,230)
-
+        
     def run_game(self):
         '''Begin the main cycle of the game'''
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                self.screen.fill (self.bg_color) 
+                self.screen.fill (self.settings.bg_color) 
                 pygame.display.flip()
 
 
---
+++ settings_1.py
@@ -0,0 +1,7 @@
+class Settings1:
+    '''Class for saving all game"s settings'''
+    def __init__(self):
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_heights = 800
+        self.bg_color = (230,230,230)
