bd0e413e5deaec8e18f5d69c39a36c47c63d1be0
Ivankalkalka <kivanka1551@gmail.com>
2022-04-11 18:47:41 +0300

    new'
    quit()

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings1()
 
+        #self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_heights = self.screen.get_rect().height
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_heights))
         pygame.display.set_caption ('Alien invasion')
         self.ship = Ship(self)
@@ -22,12 +25,32 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
         
     def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events (event)
+
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events (event)
+    
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+                
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     def _update_screen(self):
         self.screen.fill (self.settings.bg_color) 
         self.ship.blitme()
--- settings_1.py
+++ settings_1.py
@@ -5,3 +5,5 @@ class Settings1:
         self.screen_width = 1200
         self.screen_heights = 800
         self.bg_color = (230,230,230)
+        #Ship settings
+        self.ship_speed = 1.5
